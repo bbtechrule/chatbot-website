93fa4b53224d4702e07ad0ef5a656d6946b6c764
bbtechrule <bbtechrule@gmail.com>
2026-03-29 10:44:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+import os
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
